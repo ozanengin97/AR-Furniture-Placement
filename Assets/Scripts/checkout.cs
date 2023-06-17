e3487dcfa21eb7d76a529a22122058c610092957
Ozan Engin <ozan.121497@gmail.com>
2023-06-17 23:16:31 +0300

    DeepLink
    -custom android manifest file created
    -deeplink arranged for scheme
    -Created html page that mimics IKEA
    -when click the "see in the room" button, AR app opening automatically

--- Assets/Scripts/checkout.cs
+++ Assets/Scripts/checkout.cs
@@ -7,7 +7,8 @@ using TMPro;
 public class checkout : MonoBehaviour
 {
     public GameObject popupPanel;
-    public TMP_Text textComponent = null;
+    public TMP_Text textComponent;
+    private int startCounter = 0;
 
     public void OpenPopup()
     {
@@ -17,13 +18,16 @@ public class checkout : MonoBehaviour
     {
         popupPanel.SetActive(false);
     }
-    void Start()
+    public void RunStartCode()
     {
+        GameObject refComponent = GameObject.FindGameObjectWithTag("CheckoutTextComponent");
+        TMP_Text tmpComponent = refComponent.GetComponent<TMP_Text>();
+        tmpComponent.text = string.Empty;
         GameObject[] clones = GameObject.FindGameObjectsWithTag("Selectable");
         foreach (GameObject clone in clones)
         {
-            textComponent.text += " • "+clone.name.Substring(0,clone.name.Length-7).Replace("_", " ");
-            textComponent.text += "\n";
+            tmpComponent.text += " • "+clone.name.Substring(0,clone.name.Length-7).Replace("_", " ");
+            tmpComponent.text += "\n";
         }
     }
 }
